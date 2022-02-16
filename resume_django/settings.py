f0d926eb7964761f86b44f1a0c1eb99f9b29016a
Ömer Buğra Yıldırım <bugrayldrm@gmail.com>
2022-02-16 15:26:05 +0300

    template path corrected

--- resume_django/settings.py
+++ resume_django/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'resume_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'main.Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
