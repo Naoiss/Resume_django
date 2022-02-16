ecbcde9cc1a18bb51688a8f02d9d0fa3a4ffd34e
Ömer Buğra Yıldırım <bugrayldrm@gmail.com>
2022-02-16 12:13:56 +0300

    config variables added

--- resume_django/resume_django/settings.py
+++ resume_django/resume_django/settings.py
@@ -21,11 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_ee3r3j4)&xk&v4dph_)i@r28-q0+5@i)07(1k=-(h1!wsk&r4'
-#SECRET_KEY = os.environ.get('SECRET_KEY')
+#SECRET_KEY = 'django-insecure-_ee3r3j4)&xk&v4dph_)i@r28-q0+5@i)07(1k=-(h1!wsk&r4'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-#DEBUG = os.environ.get('DEBUG')
+#DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = []
 
