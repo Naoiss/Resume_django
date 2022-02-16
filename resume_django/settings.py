7fe0cc33041a7363e2df987467e5cf69c5b230eb
Ömer Buğra Yıldırım <bugrayldrm@gmail.com>
2022-02-16 17:58:53 +0300

    hosts updated

--- resume_django/settings.py
+++ resume_django/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 #DEBUG = True
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'bugra.codes']
+ALLOWED_HOSTS = ['.herokuapp.com', '.bugra.codes']
 
 
 # Application definition
