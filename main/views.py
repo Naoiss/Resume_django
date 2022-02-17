18fa076776829a2ff4074752ab02f04c03b1841b
Ömer Buğra Yıldırım <bugrayldrm@gmail.com>
2022-02-17 17:51:26 +0300

    config variables setup

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.core.mail import send_mail
-from django.conf import  settings
+from django.conf import settings
 from django.contrib import messages
 from .models import (
     UserProfile,
