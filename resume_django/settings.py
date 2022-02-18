73be131a48f9592177a580e1f02bf622bd0b1ca5
Ömer Buğra Yıldırım <bugrayldrm@gmail.com>
2022-02-18 13:01:04 +0300

    settings

--- resume_django/settings.py
+++ resume_django/settings.py
@@ -139,8 +139,8 @@ STATICFILES_DIRS = [
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "mediafiles"
+MEDIA_URL = "/static/media/"
+MEDIA_ROOT = BASE_DIR / "staticfiles/mediafiles"
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
