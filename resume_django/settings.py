1d7e4d0ee4692f3ba7aaf98e47b955c93b3c9edb
Ömer Buğra Yıldırım <bugrayldrm@gmail.com>
2022-03-04 10:46:34 +0300

    media changes

--- resume_django/settings.py
+++ resume_django/settings.py
@@ -139,8 +139,8 @@ STATICFILES_DIRS = [
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-MEDIA_URL = "/static/media/"
-MEDIA_ROOT = BASE_DIR / "staticfiles/mediafiles"
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "/mediafiles"
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
